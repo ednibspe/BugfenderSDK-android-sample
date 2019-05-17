1da53bf7a63d9cdf1e2d10cef7adaec7cd8bb59e
ednibspe <ed.oliveira@nibspe.com.br>
2019-05-17 14:40:42 -0300

    teste

--- app/src/main/java/com/bugfender/sample/BugfenderSampleApplication.java
+++ app/src/main/java/com/bugfender/sample/BugfenderSampleApplication.java
@@ -2,11 +2,14 @@ package com.bugfender.sample;
 
 import android.app.Application;
 import com.bugfender.sdk.Bugfender;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class BugfenderSampleApplication extends Application {
 
   @Override public void onCreate() {
     super.onCreate();
+    Fabric.with(this, new Crashlytics());
 
     // Initialize Bugfender
     Bugfender.init(this, "APPLICATION_TOKEN", BuildConfig.DEBUG); //TODO: replace with your app token here
